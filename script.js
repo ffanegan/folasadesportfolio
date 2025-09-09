f596ff4acb5946294a5bebd36bdd7cbadd2a8020
ffanegan <ffanegan@uchicago.edu>
2025-09-09 08:58:42 -0500

    Commit Changes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let lightmode = document.getElementsByClassName("lightmode");
 const theme = document.getElementById("theme")
 let dark = document.getElementsByTagName('darkmode')
-
+const menu_icon = document.getElementById('menu_icon')
+const hamburger = document.querySelector("#icon_three")
+const tool_bar = document.querySelector(".tool-bar")
+let openMenu = false;
+const icon_ham = document.querySelector("#icon_ham")
+const icon_close = document.querySelector("#icon_close")
 
 function light_bgd() {
     document.body.classList.toggle('lightmode')
@@ -31,6 +36,16 @@ function light_bgd() {
 //     : disableLight()
 // })
 
-function openSidebar() {}
+// hamburger.addEventListener("click", () => {})
 
-function closeSidebar() {}
+function openSidebar() {
+    tool_bar.style.right = '-2%'
+    icon_ham.style.display = "none"
+    icon_close.style.display = "inline-block"
+}
+
+function closeSidebar() {
+    tool_bar.style.right = '-100%'
+    icon_ham.style.display = "inline-block"
+    icon_close.style.display = "none"
+}
